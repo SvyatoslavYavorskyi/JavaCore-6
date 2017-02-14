ad3b1ccb19616d8836a23c3106a81e308ad8b6e6
Свят <svyatik1993@gmail.com>
2017-02-15 01:38:14 +0200

    first commit

---
+++ module3/DZ31/Bird.java
@@ -0,0 +1,13 @@
+package module3.DZ31;
+
+/**
+ * Created by Свят on 15.02.2017.
+ */
+public class Bird {
+
+    String sing;
+
+    public Bird(String sing) {
+        this.sing = sing;
+    }
+}
---
+++ module3/DZ31/Main.java
@@ -0,0 +1,21 @@
+package module3.DZ31;
+
+/**
+ * Created by Свят on 15.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Bird bird1= new Bird("I am walking");
+        Bird bird2 =new Bird("I am flying");
+        Bird bird3 = new Bird("I am singing");
+        Bird bird4 = new Bird("I am Bird");
+
+
+        System.out.println(bird1.sing);
+        System.out.println(bird2.sing);
+        System.out.println(bird3.sing);
+        System.out.println(bird4.sing);
+    }
+
+
+}
