7ff40e1ebcc9d4e683576acd59ebcc56f8722f97
Свят <svyatik1993@gmail.com>
2017-03-05 19:29:13 +0200

    first commit

---
+++ module4/ChinaBank.java
@@ -0,0 +1,37 @@
+package module4;
+
+import java.util.Currency;
+
+/**
+ * Created by Свят on 05.03.2017.
+ */
+public abstract class ChinaBank extends Bank {
+    public ChinaBank(long id, String bankCountry, Currency currency, int numberOfEmployee, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        super(id, bankCountry, currency, numberOfEmployee, avrSalaryOfEmployee, rating, totalCapital);
+    }
+
+    @Override
+    int getLimitOfWithdrawal() {
+        return 0;
+    }
+
+    @Override
+    int getLimitOfFunding() {
+        return 0;
+    }
+
+    @Override
+    int getMonthlyRate() {
+        return 0;
+    }
+
+    @Override
+    int getCommission(int summ) {
+        return 0;
+    }
+
+    @Override
+    int getPaidMonthlyForSalary() {
+        return 0;
+    }
+}
